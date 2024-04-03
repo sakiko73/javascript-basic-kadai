65209b71a5e7c1e8f245ef36aced9a8fe7bb3393
sakiko73 <159975716+sakiko73@users.noreply.github.com>
2024-04-03 23:21:08 +0900

    課題を提出いたします

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const button =document.getElementById('btn');
+const textDisplay =document.getElementById('text');
+
+
+button.addEventListener('click', function() {
+textDisplay.textContent ='ボタンをクリックしました';
+});
